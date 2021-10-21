65504e811a61611db2f1e5b104420b0d774f7af6
SpetsnazSicario <52637194+SpetsnazSicario@users.noreply.github.com>
2021-10-21 11:42:07 +0100

    Update client.ts

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,17 @@
 setImmediate(() => {
   console.log("Hello client");
 })
+
+// Haven't tested but should work
+let isToggled = false;
+RegisterCommand("example", () => {
+  if (isToggled){
+    SetNuiFocus(false, false);
+    SendNuiMessage(JSON.stringify({action: "nui-example", data: false}));
+  }else{
+    SetNuiFocus(true, true);
+    SendNuiMessage(JSON.stringify({action: "nui-example", data: true}));
+    isToggled = true;
+  }
+  
+}, false);
